844a1570462694282e5641c97475318e4e43f403
Lorenzo Romandini <lorenzoromandini@live.it>
2024-10-05 19:06:50 +0200

    Commit

--- A02/A02.py
+++ A02/A02.py
@@ -1,5 +1,3 @@
-
-import matplotlib.pyplot as plt
 import numpy as np
 
 inter_arrival_trace = np.loadtxt("Logger1.csv")
@@ -9,6 +7,7 @@ C = service_time_trace.shape[0]
 A_T = np.zeros((C, 1))
 C_T = np.zeros((C, 1))
 
+
 def calculate_metrics(alpha, beta):
     inter_arrival_new = inter_arrival_trace * alpha
     service_time_new = service_time_trace * beta
@@ -17,22 +16,22 @@ def calculate_metrics(alpha, beta):
 
     C_T[0, 0] = service_time_new[0] + A_T[0, 0]
     for i in range(1, C):
-        C_T[i, 0] = max(C_T[i-1, 0], A_T[i, 0]) + service_time_new[i]
+        C_T[i, 0] = max(C_T[i - 1, 0], A_T[i, 0]) + service_time_new[i]
 
-    lambda_ = 1 / np.mean(inter_arrival_new)
     r_i = C_T - A_T
     R = np.mean(r_i)
-    S = np.mean(service_time_new)
 
     return R
 
-def computation(fraction_alpha, fraction_beta, question): 
+
+def computation(fraction_alpha, fraction_beta, question):
     R = calculate_metrics(fraction_alpha, fraction_beta)
 
     if question == 1:
         simulation_lambda(R, fraction_alpha)
     elif question == 2:
         simulation_beta(R, fraction_alpha, fraction_beta)
+
 
 def simulation_lambda(R, alpha):
     lower_bound = 19.999
@@ -41,22 +40,23 @@ def simulation_lambda(R, alpha):
     print("Solving Exercise 1...")
     while not (lower_bound < R < upper_bound):
         if R < lower_bound:
-            alpha -= step 
+            alpha -= step
         elif R > upper_bound:
-            alpha += step  
+            alpha += step
 
         R = calculate_metrics(alpha, 1)
-    
+
     lambda_max = 1 / np.mean(inter_arrival_trace * alpha)
-    
+
     print(f"Exercise 1: R = {R}, lambda_max = {lambda_max}, alpha = {alpha}")
+
 
 def simulation_beta(R, alpha, beta):
     bound_2 = 15
     step = 0.0001
     print("Solving Exercise 2...")
     while R > bound_2:
-        beta -= step  
+        beta -= step
         R = calculate_metrics(alpha, beta)
 
     print(f"Exercise 2: R = {R}, beta = {beta}, alpha = {alpha}")
@@ -74,9 +74,3 @@ lambda_target_2 = 1.2
 fraction_alpha_2 = lambda_original / lambda_target_2
 
 computation(fraction_alpha_2, fraction_beta, 2)
-
-
-
-
-
-
--- A03/A03.py
+++ A03/A03.py
@@ -5,14 +5,15 @@ trace_1 = np.loadtxt("Trace1.csv")
 trace_2 = np.loadtxt("Trace2.csv")
 trace_3 = np.loadtxt("Trace3.csv")
 
+
 def computation(trace, i):
     print(f"**Solutions for Trace{i}:**")
     N = trace.shape[0]
 
     EX = np.mean(trace)
-    EX2 = np.mean(trace ** 2)
-    EX3 = np.mean(trace ** 3)
-    EX4 = np.mean(trace ** 4)
+    EX2 = np.mean(trace**2)
+    EX3 = np.mean(trace**3)
+    EX4 = np.mean(trace**4)
     print(f"Mean = {EX}, moment_2 = {EX2}, moment_3 = {EX3}, moment_4 = {EX4}")
 
     CX2 = np.mean((trace - EX) ** 2)
@@ -21,29 +22,33 @@ def computation(trace, i):
     print(f"Variance = {CX2}, centered_moment_3 = {CX3}, centered_moment_4 = {CX4}")
 
     sigma = np.sqrt(CX2)
-    
+
     SX3 = np.mean(((trace - EX) / sigma) ** 3)
     SX4 = np.mean(((trace - EX) / sigma) ** 4)
     print(f"Skewness = {SX3}, standardized_moment_4 = {SX4}")
 
     c_v = sigma / EX
     beta = SX4 - 3
-    print(f"Standard deviation = {sigma}, Coefficient of Variation = {c_v}, Excess Kurtosis = {beta}")
+    print(
+        f"Standard deviation = {sigma}, Coefficient of Variation = {c_v}, Excess Kurtosis = {beta}"
+    )
 
     q1 = calculate_percentile(trace, 25)
     q2 = calculate_percentile(trace, 50)
     q3 = calculate_percentile(trace, 75)
     p05 = calculate_percentile(trace, 5)
     p90 = calculate_percentile(trace, 90)
-    print(f"Median = {q2}, quartile_1 = {q1}, quartile_3 = {q3}, percentile_5 = {p05}, percentile_90 = {p90}")
+    print(
+        f"Median = {q2}, quartile_1 = {q1}, quartile_3 = {q3}, percentile_5 = {p05}, percentile_90 = {p90}"
+    )
 
     # Pearson Correlation Coefficient for lags m=1 to m=100
     x_axis = [0] * 100
     pearson_cc = [0] * 100
     for m in range(1, 101):
-        x_axis[m-1] = m
-        cross_cov = ((trace[m:] - EX)*(trace[:-m] - EX)).sum() / (N-m)
-        pearson_cc[m-1] = cross_cov/CX2
+        x_axis[m - 1] = m
+        cross_cov = ((trace[m:] - EX) * (trace[:-m] - EX)).sum() / (N - m)
+        pearson_cc[m - 1] = cross_cov / CX2
 
     plt.plot(x_axis, pearson_cc)
     plt.xticks([1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
@@ -52,27 +57,30 @@ def computation(trace, i):
     plt.show()
 
     # Approximated CDF of the corresponding distribution
-    yp = np.r_[1:N+1] / N
+    yp = np.r_[1 : N + 1] / N
     plt.plot(np.sort(trace), yp)
-    if (i == 1):
+    if i == 1:
         plt.xlim(0, 35)
-    elif (i == 2):
+    elif i == 2:
         plt.xlim(0, 6)
-    elif (i == 3): 
+    elif i == 3:
         plt.xlim(0, 140)
     plt.suptitle(f"Trace{i}: Approximated CDF")
     plt.show()
 
+
 def calculate_percentile(trace, P):
     N = trace.shape[0]
     trace = np.sort(trace)
-    h = (N-1) * P/100
-    if (h == N):
+    h = (N - 1) * P / 100
+    if h == N:
         return trace[h]
-    elif (h < N):
+    elif h < N:
         h_int = np.floor(h)
-        return trace[int(h_int)] + (h - h_int) * (trace[int(h_int)+1] - trace[int(h_int)])
+        return trace[int(h_int)] + (h - h_int) * (
+            trace[int(h_int) + 1] - trace[int(h_int)]
+        )
+
 
 for index, trace in enumerate([trace_1, trace_2, trace_3]):
-    computation(trace, index+1)
-    
+    computation(trace, index + 1)
