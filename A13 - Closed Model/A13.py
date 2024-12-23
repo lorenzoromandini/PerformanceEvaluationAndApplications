6b35ef67a40b161bbd869f49c3663fe81ab8b111
Lorenzo Romandini <lorenzoromandini@live.it>
2024-12-23 01:26:12 +0100

    Upload A14

---
+++ A13 - Closed Model/A13.py
@@ -0,0 +1,68 @@
+import numpy as np
+from scipy import linalg
+
+P = np.array(
+    [
+        [0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        # [0.05, 0.0, 0.35, 0.6, 0.0, 0.0],  we suppose that the first is the Ref station, so values in the first column must be all equal to 0.0
+        [0.0, 0.0, 0.35, 0.6, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.65, 0.35],
+        [0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.9, 0.0, 0.0, 0.0, 0.1],
+        [0.0, 0.9, 0.0, 0.0, 0.1, 0.0],
+    ]
+)
+
+l = np.zeros(6)
+l[0] = 1  # Reference station
+
+S1 = 40
+S2 = 0.05
+S3 = 0.002
+S4 = 0.08
+S5 = 0.08
+S6 = 0.1
+
+Sk = np.array([S1, S2, S3, S4, S5, S6])
+
+I = np.eye(6)
+
+v = linalg.solve((I - P).T, l)
+
+Dk = v * Sk
+print("Demands [ms]: \n\t-Disk1:", Dk[4] * 1000, "\n\t-Disk2:", Dk[5] * 1000)
+
+Z = Dk[0]
+
+N = 100
+Nk = np.zeros(6)
+Nk[0] = N  # Terminal station
+
+for n in range(1, N + 1):
+    Rk = Dk * (1 + Nk)
+    Rk[0] = 0
+
+    R = sum(Rk[1:])
+
+    X = n / (Z + R)
+
+    Nk = Rk * X
+
+    Uk = Dk * X
+
+    Xk = v * X
+
+    if n == N:
+        print("Throughput of the system:", X)
+        print("Average Response time of the system [ms]:", R * 1000)
+        print(
+            "Utilization:\n\t-Application Server:",
+            Uk[1],
+            "\n\t-DBMS:",
+            Uk[3],
+            "\n\t-Disk1:",
+            Uk[4],
+            "\n\t-Disk2:",
+            Uk[5],
+        )
+        print("Throughput:\n\t-Disk1:", Xk[4], "\n\t-Disk2:", Xk[5])
---
+++ A14 - Multi Class Open Model/A14.py
@@ -0,0 +1,109 @@
+import numpy as np
+from scipy import linalg
+
+PA = np.array(
+    [
+        [0.0, 1.0],
+        [0.1, 0.0],
+    ]
+)
+
+PB = np.array(
+    [
+        [0.0, 1.0],
+        [0.08, 0.0],
+    ]
+)
+
+PC = np.array(
+    [
+        [0.0, 1.0],
+        [0.12, 0.0],
+    ]
+)
+
+lamINA = np.zeros(2)
+lamINB = np.zeros(2)
+lamINC = np.zeros(2)
+
+lamINA[0] = 1.5 / 60
+lamINB[0] = 2.5 / 60
+lamINC[0] = 2 / 60
+
+lamA = sum(lamINA)
+lamB = sum(lamINB)
+lamC = sum(lamINC)
+
+lA = lamINA / lamA
+lB = lamINB / lamB
+lC = lamINC / lamC
+
+I = np.eye(2)
+
+vkA = linalg.solve((I - PA).T, lA)
+vkB = linalg.solve((I - PB).T, lB)
+vkC = linalg.solve((I - PC).T, lC)
+
+SkA = [8, 10]
+SkB = [3, 2]
+SkC = [4, 7]
+
+XA = lamA
+XB = lamB
+XC = lamC
+
+X = XA + XB + XC
+
+XkA = vkA * XA
+XkB = vkB * XB
+XkC = vkC * XC
+
+Xk = [XkA[0] + XkB[0] + XkC[0], XkA[1] + XkB[1] + XkC[1]]
+
+DkA = vkA * SkA
+DkB = vkB * SkB
+DkC = vkC * SkC
+
+UkA = XA * DkA
+UkB = XB * DkB
+UkC = XC * DkC
+
+Uk = [UkA[0] + UkB[0] + UkC[0], UkA[1] + UkB[1] + UkC[1]]
+print("Utilisation of the production station:", Uk[0])
+print("Utilisation of the packaging station:", Uk[1])
+
+RkA = [DkA[0] / (1 - Uk[0]), DkA[1] / (1 - Uk[1])]
+RkB = [DkB[0] / (1 - Uk[0]), DkB[1] / (1 - Uk[1])]
+RkC = [DkC[0] / (1 - Uk[0]), DkC[1] / (1 - Uk[1])]
+
+RA = RkA[0] + RkA[1]
+RB = RkB[0] + RkB[1]
+RC = RkC[0] + RkC[1]
+print("Average system response time (Class A - RA [min]):", RA)
+print("Average system response time (Class B - RB [min]):", RB)
+print("Average system response time (Class C - RC [min]):", RC)
+
+Rk = [
+    XA / X * RkA[0] + XB / X * RkB[0] + XC / X * RkC[0],
+    XA / X * RkA[1] + XB / X * RkB[1] + XC / X * RkC[1],
+]
+
+R = Rk[0] + Rk[1]
+print("Class independent average system response time (R [min]):", R)
+
+
+NkA = [XA * RkA[0], XA * RkA[1]]
+NkB = [XB * RkB[0], XB * RkB[1]]
+NkC = [XC * RkC[0], XC * RkC[1]]
+
+NA = NkA[0] + NkA[1]
+NB = NkB[0] + NkB[1]
+NC = NkC[0] + NkC[1]
+print("Average number of jobs in the system (Class A - NA):", NA)
+print("Average number of jobs in the system (Class B - NB):", NB)
+print("Average number of jobs in the system (Class C - NC):", NC)
+
+Nk = [NkA[0] + NkB[0] + NkC[0], NkA[1] + NkB[1] + NkC[1]]
+
+N = Nk[0] + Nk[1]
+print("Class independent average number of jobs in the system (N):", N)
